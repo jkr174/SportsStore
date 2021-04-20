f48832624de28dce4d898bb509c10d746f1df9ca
jkr174 <romo17410@gmail.com>
2021-04-20 17:37:07 -0500

    Update
    Finalized

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,14 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+
 using SportsStore.Models;
+
 namespace SportsStore.Controllers
+
 {
+
     public class ProductController : Controller
+
     {
+
         private IProductRepository repository;
+
         public ProductController(IProductRepository repo)
+
         {
+
             repository = repo;
+
         }
+
         public ViewResult List() => View(repository.Products);
+
     }
+
 }
