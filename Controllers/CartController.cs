018a7c1e467723af367fc37abf5a7268ccf0204b
jkr174 <romo17410@gmail.com>
2021-05-11 14:47:05 -0500

    Update
    Able to remove items from cart

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,28 +1,23 @@
 ﻿using System.Linq;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Extensions;
-using Microsoft.AspNetCore.Session;
 using Microsoft.AspNetCore.Mvc;
-using SportsStore.Infrastructure;
 using SportsStore.Models;
 using SportsStore.Models.ViewModels;
-using Newtonsoft.Json;
-
-
 namespace SportsStore.Controllers
 {
     public class CartController : Controller
     {
         private IProductRepository repository;
-        public CartController(IProductRepository repo)
+        private Cart cart;
+        public CartController(IProductRepository repo, Cart cartService)
         {
             repository = repo;
+            cart = cartService;
         }
         public ViewResult Index(string returnUrl)
         {
             return View(new CartIndexViewModel
             {
-                Cart = GetCart(),
+                Cart = cart,
                 ReturnUrl = returnUrl
             });
         }
@@ -32,11 +27,9 @@ namespace SportsStore.Controllers
             .FirstOrDefault(p => p.ProductID == productId);
             if (product != null)
             {
-                Cart cart = GetCart();
                 cart.AddItem(product, 1);
-                SaveCart(cart);
             }
-        return RedirectToAction("Index", new { returnUrl });
+            return RedirectToAction("Index", new { returnUrl });
         }
         public RedirectToActionResult RemoveFromCart(int productId,
         string returnUrl)
@@ -45,20 +38,9 @@ namespace SportsStore.Controllers
             .FirstOrDefault(p => p.ProductID == productId);
             if (product != null)
             {
-                Cart cart = GetCart();
                 cart.RemoveLine(product);
-                SaveCart(cart);
             }
             return RedirectToAction("Index", new { returnUrl });
-        }
-        private Cart GetCart()
-        {
-            Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
-            return cart;
-        }
-        private void SaveCart(Cart cart)
-        {
-            HttpContext.Session.SetJson("Cart", cart);
         }
     }
 }
